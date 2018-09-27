eeeea8ae8eba8f0a5f00aa7dcee157e91091371d
Camila Maia <cmaiacd@gmail.com>
2018-09-27 10:40:09 -0300

    Update vs-code-extensions.sh

--- vs-code-extensions.sh
+++ vs-code-extensions.sh
@@ -1,3 +1,5 @@
+code --install-extension bibhasdn.django-html
+code --install-extension bibhasdn.django-snippets
 code --install-extension ms-mssql.mssql
 code --install-extension ms-python.python
 code --install-extension WakaTime.vscode-wakatime
