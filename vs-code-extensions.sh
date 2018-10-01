4b2680e44e1a9a23ab82be2b6bc5eb06eca2943e
Camila Maia <cmaiacd@gmail.com>
2018-10-01 14:17:20 -0300

    Update vs-code-extensions.sh

--- vs-code-extensions.sh
+++ vs-code-extensions.sh
@@ -1,5 +1,8 @@
 code --install-extension bibhasdn.django-html
 code --install-extension bibhasdn.django-snippets
+code --install-extension mikestead.dotenv
 code --install-extension ms-mssql.mssql
 code --install-extension ms-python.python
+code --install-extension PeterJausovec.vscode-docker
+code --install-extension uloco.theme-bluloco-light
 code --install-extension WakaTime.vscode-wakatime
